7b6dddb6d624c1ec664887d6cce3ecc2b7618bf4
Nernay Kumar <nernaykumar98@gmail.com>
2026-01-08 16:27:50 +0530

    Add personal introduction - Nernay, Data Science Enthusiast @ Cepialabs

---
+++ pratice_project/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="card">
+        <h1>Hi, I'm Nernay</h1>
+        <p>Data Science Enthusiast</p>
+        <p>@ Cepialabs</p>
+      </div>
+    </>
+  )
+}
+
+export default App
